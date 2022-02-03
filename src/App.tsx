33d2e6975e43dc7603f97dcf1cfe36302f1a8366
Luiz Henrique <luizhjramos@outlook.com>
2022-02-03 19:37:57 -0300

    feat: initial form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
-
+import React, { useState } from "react";
 
 const App: React.FC = () => {
-  return <div>Olá Mundo</div>
-}
+  // -------------------------------------------------
+  // State
+  // -------------------------------------------------
+  const [search, setSearch] = useState("");
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  return (
+    <div>
+      <h1>Search</h1>
+
+      <form>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+};
 
 export default App;
